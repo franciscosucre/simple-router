2ef2e363f83521409a1ad71e0fa36156da8356f9
franciscosucre <franciscojsucreg@gmail.com>
2019-02-03 15:12:40 +0100

    sda

--- src/test/Router.test.ts
+++ src/test/Router.test.ts
@@ -1,37 +1,37 @@
-import * as chai from 'chai'
-import * as http from 'http'
-import * as assert from 'assert'
+import * as assert from 'assert';
+import * as chai from 'chai';
+import * as http from 'http';
 import Router, { IDynamicRequest } from '..';
 const AssertionError = assert.AssertionError;
 let router: Router;
-const OPTIONS = "OPTIONS";
-const HEAD = "HEAD";
-const GET = "GET";
-const POST = "POST";
-const PUT = "PUT";
-const PATCH = "PATCH";
-const DELETE = "DELETE";
+const OPTIONS = 'OPTIONS';
+const HEAD = 'HEAD';
+const GET = 'GET';
+const POST = 'POST';
+const PUT = 'PUT';
+const PATCH = 'PATCH';
+const DELETE = 'DELETE';
 
-const PATH = "/foo";
-const PATH_WITH_ARGUMENTS = "/:foo/:fighters";
-const headers = { "Content-Type": "application/json" };
+const PATH = '/foo';
+const PATH_WITH_ARGUMENTS = '/:foo/:fighters';
+const headers = { 'Content-Type': 'application/json' };
 const SIMPLE_HANDLER = (req: IDynamicRequest, res: http.ServerResponse) => {
   res.writeHead(200, headers);
   res.end(
     JSON.stringify({
-      params: req.params
-    })
+      params: req.params,
+    }),
   );
 };
 const SIMPLE_MIDDLEWARE = (req: IDynamicRequest, res: http.ServerResponse) => {
   req.middlwarePassed = true;
 };
-const chaiHttp = require("chai-http");
+import chaiHttp = require('chai-http');
 chai.use(chaiHttp);
 chai.should();
 
-//Our parent block
-describe("Simple NodeJS Router", () => {
+// Our parent block
+describe('Simple NodeJS Router', () => {
   beforeEach(async () => {
     router = new Router();
   });
@@ -41,22 +41,21 @@ describe("Simple NodeJS Router", () => {
   });
 
   describe(`Constructor`, () => {
-    it("It should create a router without arguments", async () => {
+    it('It should create a router without arguments', async () => {
       const fn = () => new Router();
       fn.should.not.throw(Error);
     });
   });
 
   describe(`addRoute`, () => {
-
-    it("It should create a route with two handlers if try to add a duplicate route", async () => {
+    it('It should create a route with two handlers if try to add a duplicate route', async () => {
       router.addRoute(GET, PATH, SIMPLE_HANDLER);
       router.addRoute(GET, PATH, SIMPLE_HANDLER);
       router.routes.length.should.be.eql(1);
       router.routes[0].handlers.length.should.be.eql(2);
     });
 
-    it("It create a route", async () => {
+    it('It create a route', async () => {
       router.addRoute(GET, PATH, SIMPLE_HANDLER);
       const routes = router.routes;
       routes.length.should.be.eql(1);
@@ -66,7 +65,7 @@ describe("Simple NodeJS Router", () => {
       createdRoute.handlers[0].should.be.eql(SIMPLE_HANDLER);
     });
 
-    it("It create a route with keys if given a path with arguments", async () => {
+    it('It create a route with keys if given a path with arguments', async () => {
       router.addRoute(GET, PATH_WITH_ARGUMENTS, SIMPLE_HANDLER);
       const routes = router.routes;
       routes.length.should.be.eql(1);
@@ -75,12 +74,12 @@ describe("Simple NodeJS Router", () => {
       createdRoute.path.should.be.eql(PATH_WITH_ARGUMENTS);
       createdRoute.handlers[0].should.be.eql(SIMPLE_HANDLER);
       const keyStrings = createdRoute.keys.map(k => k.name);
-      keyStrings.should.be.eql(["foo", "fighters"]);
+      keyStrings.should.be.eql(['foo', 'fighters']);
     });
 
     describe(`addRoute Aliases`, () => {
       describe(`OPTIONS`, () => {
-        it("It should add a route with a OPTIONS method", async () => {
+        it('It should add a route with a OPTIONS method', async () => {
           router.options(PATH, SIMPLE_HANDLER);
           const routes = router.routes;
           routes.length.should.be.eql(1);
@@ -92,7 +91,7 @@ describe("Simple NodeJS Router", () => {
       });
 
       describe(`HEAD`, () => {
-        it("It should add a route with a HEAD method", async () => {
+        it('It should add a route with a HEAD method', async () => {
           router.head(PATH, SIMPLE_HANDLER);
           const routes = router.routes;
           routes.length.should.be.eql(1);
@@ -104,7 +103,7 @@ describe("Simple NodeJS Router", () => {
       });
 
       describe(`GET`, () => {
-        it("It should add a route with a GET method", async () => {
+        it('It should add a route with a GET method', async () => {
           router.get(PATH, SIMPLE_HANDLER);
           const routes = router.routes;
           routes.length.should.be.eql(1);
@@ -116,7 +115,7 @@ describe("Simple NodeJS Router", () => {
       });
 
       describe(`POST`, () => {
-        it("It should add a route with a POST method", async () => {
+        it('It should add a route with a POST method', async () => {
           router.post(PATH, SIMPLE_HANDLER);
           const routes = router.routes;
           routes.length.should.be.eql(1);
@@ -128,7 +127,7 @@ describe("Simple NodeJS Router", () => {
       });
 
       describe(`PUT`, () => {
-        it("It should add a route with a PUT method", async () => {
+        it('It should add a route with a PUT method', async () => {
           router.put(PATH, SIMPLE_HANDLER);
           const routes = router.routes;
           routes.length.should.be.eql(1);
@@ -140,7 +139,7 @@ describe("Simple NodeJS Router", () => {
       });
 
       describe(`PATCH`, () => {
-        it("It should add a route with a PATCH method", async () => {
+        it('It should add a route with a PATCH method', async () => {
           router.patch(PATH, SIMPLE_HANDLER);
           const routes = router.routes;
           routes.length.should.be.eql(1);
@@ -152,7 +151,7 @@ describe("Simple NodeJS Router", () => {
       });
 
       describe(`DELETE`, () => {
-        it("It should add a route with a DELETE method", async () => {
+        it('It should add a route with a DELETE method', async () => {
           router.delete(PATH, SIMPLE_HANDLER);
           const routes = router.routes;
           routes.length.should.be.eql(1);
@@ -164,7 +163,7 @@ describe("Simple NodeJS Router", () => {
       });
 
       describe(`ALL`, () => {
-        it("It should add a route for several methods method", async () => {
+        it('It should add a route for several methods method', async () => {
           router.all(PATH, SIMPLE_HANDLER);
           const routes = router.routes;
           routes.length.should.be.eql(7);
@@ -174,33 +173,35 @@ describe("Simple NodeJS Router", () => {
   });
 
   describe(`match`, () => {
-    it("It should return the route if it exists", async () => {
+    it('It should return the route if it exists', async () => {
       router.get(PATH, SIMPLE_HANDLER);
       const route = router.match(GET, PATH);
-      if (!route) throw new Error("Test gone wrong!")
-      route.should.have.property("method");
-      route.should.have.property("path");
-      route.should.have.property("handlers");
-      route.should.have.property("keys");
-      route.should.have.property("regex");
+      if (!route) {
+        throw new Error('Test gone wrong!');
+      }
+      route.should.have.property('method');
+      route.should.have.property('path');
+      route.should.have.property('handlers');
+      route.should.have.property('keys');
+      route.should.have.property('regex');
     });
 
-    it("It should return null if the route does not exist", async () => {
+    it('It should return null if the route does not exist', async () => {
       const exists = router.match(GET, PATH) !== null;
       exists.should.be.eql(false);
     });
   });
 
   describe(`useMiddleware`, () => {
-    it("It should throw an exception if no handlers are given", async () => {
+    it('It should throw an exception if no handlers are given', async () => {
       const fn = () => router.useMiddleware();
       fn.should.throw(AssertionError);
     });
 
-    it("It should add the middleware before the router handler", async () => {
+    it('It should add the middleware before the router handler', async () => {
       router.useMiddleware(SIMPLE_MIDDLEWARE);
-      router.get("/foo", SIMPLE_HANDLER);
-      router.post("/fighter", SIMPLE_HANDLER);
+      router.get('/foo', SIMPLE_HANDLER);
+      router.post('/fighter', SIMPLE_HANDLER);
       router.routes.length.should.be.eql(2);
       const [get, post] = router.routes;
       get.handlers.length.should.be.eql(2);
@@ -213,61 +214,61 @@ describe("Simple NodeJS Router", () => {
   });
 
   describe(`useSubrouter`, () => {
-
-    it("It should append all the secondRouter routes to the main router", async () => {
+    it('It should append all the secondRouter routes to the main router', async () => {
       const secondRouter = new Router();
-      secondRouter.get("/foo", SIMPLE_HANDLER);
-      secondRouter.get("/fighter", SIMPLE_HANDLER);
-      router.useSubrouter("/second", secondRouter);
+      secondRouter.get('/foo', SIMPLE_HANDLER);
+      secondRouter.get('/fighter', SIMPLE_HANDLER);
+      router.useSubrouter('/second', secondRouter);
       router.routes.length.should.be.eql(2);
-      router.routes[0].path.should.be.eql("/second/foo");
-      router.routes[1].path.should.be.eql("/second/fighter");
+      router.routes[0].path.should.be.eql('/second/foo');
+      router.routes[1].path.should.be.eql('/second/fighter');
     });
 
-    it("It should append all the secondRouter routes to the main router with the main router middleware", async () => {
+    it('It should append all the secondRouter routes to the main router with the main router middleware', async () => {
       router.useMiddleware(SIMPLE_MIDDLEWARE);
       const secondRouter = new Router();
-      secondRouter.get("/foo", SIMPLE_HANDLER);
-      secondRouter.get("/fighter", SIMPLE_HANDLER);
-      router.useSubrouter("/second", secondRouter);
+      secondRouter.get('/foo', SIMPLE_HANDLER);
+      secondRouter.get('/fighter', SIMPLE_HANDLER);
+      router.useSubrouter('/second', secondRouter);
       router.routes.length.should.be.eql(2);
       router.routes[0].handlers[0].should.be.eql(SIMPLE_MIDDLEWARE);
       router.routes[1].handlers[0].should.be.eql(SIMPLE_MIDDLEWARE);
     });
 
     it(
-      "It should append all the secondRouter routes to the main router with " +
-        "the second router middleware, without affecting the main route old routes",
+      'It should append all the secondRouter routes to the main router with ' +
+        'the second router middleware, without affecting the main route old routes',
       async () => {
-        router.get("/first", SIMPLE_HANDLER);
+        router.get('/first', SIMPLE_HANDLER);
         const secondRouter = new Router();
         secondRouter.useMiddleware(SIMPLE_MIDDLEWARE);
-        secondRouter.get("/foo", SIMPLE_HANDLER);
-        secondRouter.get("/fighter", SIMPLE_HANDLER);
-        router.useSubrouter("/second", secondRouter);
+        secondRouter.get('/foo', SIMPLE_HANDLER);
+        secondRouter.get('/fighter', SIMPLE_HANDLER);
+        router.useSubrouter('/second', secondRouter);
         router.routes.length.should.be.eql(3);
         router.routes[0].handlers.length.should.be.eql(1);
         router.routes[1].handlers.length.should.be.eql(2);
         router.routes[2].handlers.length.should.be.eql(2);
-      }
+      },
     );
   });
 
   describe(`handle`, () => {
-    const server = http.createServer(async (req: http.IncomingMessage,res: http.ServerResponse) => {
+    const server = http.createServer(async (req: http.IncomingMessage, res: http.ServerResponse) => {
       try {
-        if (!router.match(req.method || '', req.url || ''))
-          throw { name: "ResourceNotFound", message: "Resource not found", status: 404 };
+        if (!router.match(req.method || '', req.url || '')) {
+          throw { name: 'ResourceNotFound', message: 'Resource not found', status: 404 };
+        }
         await router.handle(req as IDynamicRequest, res);
       } catch (error) {
         const status = error.status || 500;
         res.writeHead(status, headers);
         res.end(
           JSON.stringify({
-            code: error.code || error.name || "Error",
-            message: error.message || "An unexpected error has ocurred",
-            status
-          })
+            code: error.code || error.name || 'Error',
+            message: error.message || 'An unexpected error has ocurred',
+            status,
+          }),
         );
       }
     });
@@ -280,13 +281,13 @@ describe("Simple NodeJS Router", () => {
       server.close();
     });
 
-    it("It should run the handlers", async () => {
+    it('It should run the handlers', async () => {
       router.get(PATH, SIMPLE_HANDLER);
       const response = await chai.request(server).get(PATH);
       response.should.have.status(200);
     });
 
-    it("It should run the middleware first and then the handlers", async () => {
+    it('It should run the middleware first and then the handlers', async () => {
       router.useMiddleware((req, res) => {
         req.middleware = true;
       });
@@ -299,15 +300,15 @@ describe("Simple NodeJS Router", () => {
       response.should.have.status(200);
     });
 
-    it("It should run the handlers and store the arguments if a route with arguments was given", async () => {
+    it('It should run the handlers and store the arguments if a route with arguments was given', async () => {
       router.get(PATH_WITH_ARGUMENTS, SIMPLE_HANDLER);
-      const response = await chai.request(server).get("/hello/world");
+      const response = await chai.request(server).get('/hello/world');
       response.should.have.status(200);
-      response.body.should.have.property("params");
-      response.body.params.should.have.property("foo");
-      response.body.params.should.have.property("fighters");
-      response.body.params.foo.should.be.eql("hello");
-      response.body.params.fighters.should.be.eql("world");
+      response.body.should.have.property('params');
+      response.body.params.should.have.property('foo');
+      response.body.params.should.have.property('fighters');
+      response.body.params.foo.should.be.eql('hello');
+      response.body.params.fighters.should.be.eql('world');
     });
   });
 });
