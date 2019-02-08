661d15dc77a6be1c720902528880a31118827f44
Francisco Javier Sucre González <30059200@hq.es.corp.leroymerlin.com>
2019-02-08 13:04:30 +0100

    sdfdsf

--- src/interfaces.ts
+++ src/interfaces.ts
@@ -2,7 +2,12 @@ import { IncomingMessage, ServerResponse } from 'http';
 
 export type HandlerFunction = (req: IRequest, res: IResponse, next?: INextFunction) => void;
 export type INextFunction = () => any;
-export interface IRequest extends IncomingMessage {
+export interface IRequest {
+  url?: string;
+  method?: string;
+  params?: {
+    [key: string]: any;
+  };
   [key: string]: any;
 }
 export interface IResponse extends ServerResponse {
